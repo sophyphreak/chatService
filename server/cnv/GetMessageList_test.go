9227482efc05142defd8c00ee4509cb55da93395
Andrew Horn <arhorn@smcm.edu>
2020-07-08 15:35:09 -0500

    finished creating GetMessageList testing

--- server/cnv/GetMessageList_test.go
+++ server/cnv/GetMessageList_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGetMessages(t *testing.T) {
-	defer func() { Conversations = Conversations[:0] }()
+	defer clearConversations()
 
 	var m msg.Message
 	m.Username = "bali"
@@ -20,4 +20,15 @@ func TestGetMessages(t *testing.T) {
 
 	Conversations = append(Conversations, c)
 
+	var messages []msg.Message
+	messages = GetMessageList(c.User2, c.User1)
+	if len(messages) != 1 {
+		t.Errorf("Expected length of messages to be 1 but instead received %d", len(messages))
+	}
+	if messages[0].Username != c.User1 {
+		t.Errorf("Expected %s as messsage username but instead received %s", c.User1, messages[0].Username)
+	}
+	if messages[0].Body != m.Body {
+		t.Errorf("Expected %s as message body but instead received %s", m.Body, messages[0].Body)
+	}
 }
