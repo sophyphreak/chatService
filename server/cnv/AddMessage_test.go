9dde524f0d38e43a4e84685b90e8edefe8717ec5
baliwoods <balihehar@gmail.com>
2020-07-08 12:59:36 -0700

    updated Add Message

--- server/cnv/AddMessage_test.go
+++ server/cnv/AddMessage_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestAddMessage(t *testing.T) {
-
+	defer clearConversations()
 	t.Run("new Convo", func(t *testing.T) {
 		user1 := "bhehar"
 		user2 := "andrew"
@@ -19,7 +19,7 @@ func TestAddMessage(t *testing.T) {
 		//adds message to global var Conversations
 		AddMessage(user1, user2, newMsg)
 		//reset global var Conversations to empty
-		defer func() { Conversations = Conversations[:0] }()
+		// defer func() { Conversations = Conversations[:0] }()
 
 		got := Conversations[0]
 		expected := Conversation{user2, user1, messageSlice}
@@ -35,6 +35,33 @@ func TestAddMessage(t *testing.T) {
 		if reflect.DeepEqual(got.Messages, expected.Messages) == false {
 			t.Errorf("Got messages: %v Expected message: %v", got, expected)
 		}
+	})
+
+	t.Run("existing convo", func(t *testing.T) {
+
+		user1 := "bhehar"
+		user2 := "andrew"
+		newMsg1 := msg.CreateMessage(user1, "what's up dude")
+		newMsg2 := msg.CreateMessage(user2, "not much breh, jus hangin")
+		messageSlice := make([]msg.Message, 0)
+		messageSlice = append(messageSlice, newMsg1, newMsg2)
+
+		expected := Conversation{user2, user1, messageSlice}
+
+		AddMessage(user1, user2, newMsg2)
+		got := Conversations[0]
+		if got.User1 != expected.User1 {
+			t.Errorf("Got User 1: %v Expected User 1: %v", got, expected)
+		}
+
+		if got.User2 != expected.User2 {
+			t.Errorf("Got User 2: %v Expected User 2: %v", got, expected)
+		}
+
+		if reflect.DeepEqual(got.Messages, expected.Messages) == false {
+			t.Errorf("Got messages: %v Expected message: %v", got, expected)
+		}
+
 	})
 
 }
