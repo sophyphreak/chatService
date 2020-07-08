3cad5a779b194ed69c4366dc892b857d9d25738f
Jasmin Smith <jasmith.07.13.99@gmail.com>
2020-07-08 19:35:43 -0400

    sorry

--- server/ch/AddMessage_test.go
+++ server/ch/AddMessage_test.go
@@ -10,11 +10,6 @@ import (
 // Tests to see if adds Message correctly
 func TestAddMessage(t *testing.T) {
 
-	var ch Channel
-
-	fmt.Println(ch.Messages)
-	empty := len(ch.Messages)
-
 	newMessage := msg.Message{
 		"jazzyjazz0713",
 		"I am sick and tired of being sick and tired.",
@@ -22,9 +17,9 @@ func TestAddMessage(t *testing.T) {
 	AddMessage(newMessage)
 
 	fmt.Println()
-	hopeful := len(ch.Messages)
+	hopeful := len(Channel.Messages)
 
-	fmt.Println("This is channel after adding message:", ch.Messages)
+	fmt.Println("This is channel after adding message:", Channel.Messages)
 
 	fmt.Println(empty)
 	if empty == hopeful {
