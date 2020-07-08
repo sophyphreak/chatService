c72f8b2d3f32d6c3195703e64471cdd3f2bad484
Andrew Horn <arhorn@smcm.edu>
2020-07-08 13:16:28 -0500

    working on getChannelMessages

--- server/api/getChannelMessages.go
+++ server/api/getChannelMessages.go
@@ -1,7 +1,27 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func getChannelMessages(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user1 := vars["user1"]
+	user2 := vars["user2"]
+	fmt.Println(user1)
+	fmt.Println(user2)
 
+	// gameIdInt, _ := strconv.Atoi(gameId)
+	// foundGame, err := game.GetGame(gameIdInt)
+	// game.Check(err)
+	// json.NewEncoder(w).Encode(foundGame)
+
+	// receives
+	//  - channel name
+
+	// returns
+	//  - slice of messages
 }
--- server/api/handleRequests.go
+++ server/api/handleRequests.go
@@ -6,8 +6,8 @@ import (
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/message/channel", getChannelMessages).Methods("GET")
-	router.HandleFunc("/message/direct", getDirectMessages).Methods("GET")
+	router.HandleFunc("/message/channel/{channelName}", getChannelMessages).Methods("GET")
+	router.HandleFunc("/message/direct/{user1}/{user2}", getDirectMessages).Methods("GET")
 	router.HandleFunc("/channel", getChannels).Methods("GET")
 	router.HandleFunc("/username", getUsernames).Methods("GET")
 	router.HandleFunc("/message/channel", addChannelMessage).Methods("POST")
