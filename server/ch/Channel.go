b9d87ac77598c783ce935f3f9c3f318d65a59e91
Andrew Horn <arhorn@smcm.edu>
2020-07-08 11:27:49 -0500

    added notes for what to build

--- server/ch/Channel.go
+++ server/ch/Channel.go
@@ -3,10 +3,28 @@ package ch
 import "../msg"
 
 // Channels is a slice of all Channels
-var Channels []Channel
+var Channels []*Channel
 
 // Channel is a channel
 type Channel struct {
 	Name     string        `json:"name"`
 	Messages []msg.Message `json:"messages"`
 }
+
+// CreateChannel function
+//	- create channel with name, empty slice
+//	- check if channel is unqiue, if not add timestamp
+//	- add to the Channels slice
+
+// AddMessage function
+//	- add sent message to Messages slice
+
+// GetMessages function
+//	- takes in channel name
+//	- returns slice of all messages in that channel
+
+// clearChannels function
+//	- sets Channels to empty slice
+
+// GetChannelNames function
+//	- returns slice of all channel names
--- server/cnv/Conversation.go
+++ server/cnv/Conversation.go
@@ -2,9 +2,23 @@ package cnv
 
 import "../msg"
 
+// Conversations is a slice of Conversation
+var Conversations []Conversation
+
 // Conversation is a private conversation between two Users
 type Conversation struct {
 	User1    string        `json:"user1"`
 	User2    string        `json:"user2"`
 	Messages []msg.Message `json:"messages"`
 }
+
+// AddMessage function
+//	- checks if Conversation already exists, if not, creates it w/ User1, User2, empty messages slice
+//	- add sent message to Messages slice
+
+// GetMessageList function
+// 	- checks if Conversation exists with User1 and User2
+//	- returns all messages if does exist
+
+// GetUsers function
+//	- returns the two users for conversation
