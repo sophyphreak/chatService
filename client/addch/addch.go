40480e76704c7fe3fc042d197a115e3d3f306e9e
Andrew Horn <arhorn@smcm.edu>
2020-07-08 21:44:03 -0500

    finished addch functionality

--- client/addch/addch.go
+++ client/addch/addch.go
@@ -1,6 +1,34 @@
 package addch
 
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
 // asks user for name
 // sends POST to /channel
 // tells user if it was created successfully or if already existed
 // send user to main menu
+
+// Begin is the entrypoint for the add channel functionality
+func Begin() {
+	fmt.Print("Enter new channel name: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	channelName := scanner.Text()
+
+	values := map[string]string{"name": channelName}
+	jsonValue, _ := json.Marshal(values)
+	_, err := http.Post("http://localhost:10000/channel", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Was not able to reach server")
+		return
+	}
+	fmt.Println("Your channel", channelName, "was created successfully!")
+	return
+}
