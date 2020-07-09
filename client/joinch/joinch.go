d11bc8e7463f33ca503732e766702e007d946363
Andrew Horn <arhorn@smcm.edu>
2020-07-08 21:45:29 -0500

    started adding joinch

--- client/joinch/joinch.go
+++ client/joinch/joinch.go
@@ -1,5 +1,15 @@
 package joinch
 
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
 // display list of channels
 // user inputs what channel they want to join
 // display all messages in channel
@@ -8,3 +18,24 @@ package joinch
 // sends a GET request to /messages/channel/{channelName} every .25 seconds
 //		- adds any messages that aren't already displayed
 // allows user to quit to main menu by typing "quit" or something
+
+// Begin is the entry point to join channel functionality
+func Begin() {
+	var channels []string
+	res, err := http.Get("http://localhost:10000/channel")
+	if err != nil {
+		log.Fatal(err)
+	}
+	reqBody, err := ioutil.ReadAll(res.Body)
+	json.Unmarshal(reqBody, &channels)
+	fmt.Println("List of channels:")
+	for _, c := range channels {
+		fmt.Println(c)
+	}
+	fmt.Print("Which channel? ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	channelName := scanner.Text()
+	fmt.Println(channelName)
+
+}
