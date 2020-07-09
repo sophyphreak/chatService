dcb73d5ba49969b387b2636ecf6330aecbdaa15a
Andrew Horn <arhorn@smcm.edu>
2020-07-08 19:41:48 -0500

    removed unused params from POST direct message

--- server/api/HandleRequests.go
+++ server/api/HandleRequests.go
@@ -15,7 +15,7 @@ func HandleRequests() {
 	router.HandleFunc("/channel", getChannels).Methods("GET")
 	router.HandleFunc("/username", getUsernames).Methods("GET")
 	router.HandleFunc("/message/channel", addChannelMessage).Methods("POST")
-	router.HandleFunc("/message/direct/{user1}/{user2}", addDirectMessage).Methods("POST")
+	router.HandleFunc("/message/direct", addDirectMessage).Methods("POST")
 	router.HandleFunc("/channel", addChannel).Methods("POST")
 	router.HandleFunc("/username", addUser).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", router))
