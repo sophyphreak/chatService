37c6a0e0e2ae8bb2ae05e7dcc5203cf4bb8fcbc9
Andrew Horn <arhorn@smcm.edu>
2020-07-08 22:37:38 -0500

    getChannelMessages now returns json and not slice

--- server/api/getChannelMessages.go
+++ server/api/getChannelMessages.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 
 	"../ch"
+	"../msg"
 	"github.com/gorilla/mux"
 )
+
+type response struct {
+	Messages []msg.Message `json:"messages"`
+}
 
 func getChannelMessages(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	channelName := vars["channelName"]
-	messages := ch.GetMessages(channelName)
+	msgs := ch.GetMessages(channelName)
+	var messages response
+	messages.Messages = msgs
 	json.NewEncoder(w).Encode(messages)
 }
