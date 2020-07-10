faf78e371d31eefb641d449dd53873574b3a6c40
Andrew Horn <arhorn@smcm.edu>
2020-07-10 11:04:14 -0500

    refactoring, separating out functions, converting for to goto Top

--- client/joinch/chooseChannel.go
+++ client/joinch/chooseChannel.go
@@ -9,18 +9,18 @@ import (
 )
 
 func chooseChannel(channels []string) string {
-	for {
-		fmt.Println("List of channels:")
-		for _, c := range channels {
-			fmt.Println(c)
-		}
-		fmt.Print("Which channel? ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		channelName := scanner.Text()
-		if utils.StringInSlice(channelName, channels) {
-			return channelName
-		}
-		fmt.Println("That is not an available channel.")
+	scanner := bufio.NewScanner(os.Stdin)
+Top:
+	fmt.Println("List of channels:")
+	for _, c := range channels {
+		fmt.Println(c)
 	}
+	fmt.Print("Which channel? ")
+	scanner.Scan()
+	channelName := scanner.Text()
+	if utils.StringInSlice(channelName, channels) {
+		return channelName
+	}
+	fmt.Println("That is not an available channel.")
+	goto Top
 }
--- client/joinch/inputMessages.go
+++ client/joinch/inputMessages.go
@@ -13,24 +13,28 @@ import (
 )
 
 func inputMessages(currentChannel, username string) {
-	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		message := strings.TrimSpace(scanner.Text())
-		if message == "quit" {
-			return
-		}
-		values := make(map[string]string)
-		values["channelName"] = currentChannel
-		values["username"] = username
-		values["body"] = message
-		jsonValue, err := json.Marshal(values)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = http.Post(utils.URL+"/message/channel", "application/json", bytes.NewBuffer(jsonValue))
-		if err != nil {
-			log.Fatal(err)
-		}
+	scanner := bufio.NewScanner(os.Stdin)
+Top:
+	scanner.Scan()
+	message := strings.TrimSpace(scanner.Text())
+	if message == "quit" {
+		return
+	}
+	sendMessage(currentChannel, username, message)
+	goto Top
+}
+
+func sendMessage(currentChannel, username, message string) {
+	values := make(map[string]string)
+	values["channelName"] = currentChannel
+	values["username"] = username
+	values["body"] = message
+	jsonValue, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = http.Post(utils.URL+"/message/channel", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Fatal(err)
 	}
 }
